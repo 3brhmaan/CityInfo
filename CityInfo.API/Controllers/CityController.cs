faf208339d42de9a2e19f2e15b4124606fab746a
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2024-04-28 00:54:13 +0300

    adding ability to get point of iterest of every city

---
+++ CityInfo.API/Controllers/CityController.cs
@@ -0,0 +1,28 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/Cities")]
+[ApiController]
+public class CityController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<CityDto>> GetCities()
+    {
+        return Ok(CityDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<CityDto> GetCity(int id)
+    {
+        var city = CityDataStore.Current.Cities
+            .FirstOrDefault(c => c.Id == id);
+
+        if(city is null)
+            return NotFound();
+        else
+            return Ok(city);
+    }
+}
